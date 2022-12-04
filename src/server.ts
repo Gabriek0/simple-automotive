e34909a97cdfc192e6fb3cebd0aa90b0683c313d
Gabriel Henrique <gabriel.sanches170@gmail.com>
2022-12-03 23:34:55 -0300

    feat: config debug

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-    return res.json({ message: "Hello!" })
-})
+  return res.json({ message: "Hello!" });
+});
+
+app.post("/courses", (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
 
 app.listen(3333, () => console.log("Server is running!"));
