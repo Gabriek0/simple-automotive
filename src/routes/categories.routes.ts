7afc62be7422eb7bf67461c08faa42ef12ea2d6a
Gabriel Henrique <gabriel.sanches170@gmail.com>
2022-12-06 22:22:27 -0300

    feat: create method to list all categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,14 +5,18 @@ import { CategoriesRepository } from "../repositories/CategoriesRepository";
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
 categoriesRoutes.post("/", (req: Request, res: Response) => {
   const { name, description } = req.body;
 
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req: Request, res: Response) => {
+  const categories = categoriesRepository.list();
+
+  return res.json(categories);
 });
 
 export { categoriesRoutes };
