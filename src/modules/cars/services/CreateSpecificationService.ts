4f2466923ec72603944a3253fcf88bf73823e279
Gabriel Henrique <gabriel.sanches170@gmail.com>
2022-12-09 16:00:59 -0300

    feat: add CreateSpecificationService, route and interface

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("TODO");
+  constructor(private specificationRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationNameAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationNameAlreadyExists) {
+      throw new Error("Specification name already exists");
+    }
+
+    this.specificationRepository.create({
+      name,
+      description,
+    });
   }
 }
 
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,20 @@
+import { Router, Response, Request } from "express";
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (req: Request, res: Response) => {
+  const { name, description } = req.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return res.status(201).send();
+});
+
+export { specificationsRoutes };
