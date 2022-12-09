47a21dc3b3d63a444b736d9cac94e8ce401d8b2c
Gabriel Henrique <gabriel.sanches170@gmail.com>
2022-12-09 11:19:40 -0300

    feat: apply liskov substitution principle

---
+++ src/repositories/ICategoriesRepository.ts
@@ -0,0 +1,14 @@
+import { Category } from "../models/Category";
+
+export interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+interface ICategoriesRepository {
+  list(): Category[];
+  findByName(name: string): Category;
+  create({ name, description }: ICreateCategoryDTO): void;
+}
+
+export { ICategoriesRepository };
---
+++ src/repositories/PostgresCategoriesRepository.ts
@@ -0,0 +1,23 @@
+import { Category } from "../models/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+class PostgresCategoriesRepository implements ICategoriesRepository {
+  create({ name, description }: ICreateCategoryDTO): void {
+    console.log(name, description);
+    return null;
+  }
+
+  findByName(name: string): Category {
+    console.log(name);
+    return null;
+  }
+
+  list(): Category[] {
+    return null;
+  }
+}
+
+export { PostgresCategoriesRepository };
