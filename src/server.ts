f3d79c8c944e1147d2eb68791ca4902acf7a6928
Gabriel Henrique <gabriel.sanches170@gmail.com>
2022-12-18 13:43:18 -0300

    feat: add swagger.json with post and get methods in /categories route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 import { router } from "./routes";
+
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+// /api-docs -> route
+// swaggerUI.serve -> run server
+// swaggerUI.setup() -> run setup of documentation
+
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
