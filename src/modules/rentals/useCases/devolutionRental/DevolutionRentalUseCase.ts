a4648815e8c38d44aa22cee6d16c7a877502e581
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-03-09 21:15:08 -0300

    :hammer: feat: Create devolution rental use case

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -23,12 +23,47 @@ class DevolutionRentalUseCase {
 
   async execute({ id, user_id }: IRequest) {
     const rental = await this.rentalsRepository.findById(id);
+    const car = await this.carsRepository.findById(id);
+    let minimum_daily = 1;
 
     if (!rental) {
       throw new AppError("Rental does not exist!");
     }
 
-    // Verificar o tempo de aluguel
+    const dateNow = this.dayjsDateProvider.dateNow();
+
+    // number of rental daily
+    let daily = this.dayjsDateProvider.compareInDays(
+      rental.start_date,
+      dateNow
+    );
+
+    if (daily <= 0) {
+      daily = minimum_daily;
+    }
+
+    // days of rentals delay
+    const delay = this.dayjsDateProvider.compareInDays(
+      dateNow,
+      rental.expected_return_date
+    );
+
+    let total = 0;
+
+    if (delay > 0) {
+      const calculate_fine = delay * car.fine_amount;
+      total = calculate_fine;
+    }
+
+    total += daily * car.daily_rate;
+
+    rental.end_date = this.dayjsDateProvider.dateNow();
+    rental.total = total;
+
+    await this.rentalsRepository.create(rental);
+    await this.carsRepository.updateAvailable(car.id, true);
+
+    return rental;
   }
 }
 
